ddbbd0998a11079fd08f9fe175126aa0e45f62a7
Karurragul <karurragul@gmail.com>
2019-05-09 08:56:46 +0530

    changed now

--- src/org/test/Number123.java
+++ src/org/test/Number123.java
@@ -5,6 +5,7 @@ public class Number123 {
 		for (int i = 5; i <=1; i++) {
 			for (int j = 1; j <=i; j++) {
 				System.out.print("*");
+				System.out.println("______");
 			}
 			System.out.println();
 		}
